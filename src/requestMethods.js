ec7d6e78a8572def0798255bb6b78118ef319c8b
Ankush Gupta <ankushgupta365@gmail.com>
2023-01-26 18:04:12 +0530

    requestmethod.js null token fromm localstorage bug fixed

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URL = "https://devshop-api.onrender.com/api"
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+const TOKEN = ()=>{
+    return (localStorage.getItem("persist:root").user && JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken)
+}
 
 
 export const publicRequest = axios.create({
